cf8446c1a8cf553ba5d5cd2c16c5a92ece57ced8
Dan Hatfield <danhat1020@gmail.com>
2025-08-22 00:20:11 +0100

    Changed neovim colour scheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -57,6 +57,7 @@ vim.pack.add({
     { src = "https://github.com/nvim-tree/nvim-web-devicons" },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/EdenEast/nightfox.nvim" },
+    { src = "https://github.com/vague2k/vague.nvim" },
     { src = "https://github.com/stevearc/oil.nvim" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
--- .config/nvim/lua/core/colors.lua
+++ .config/nvim/lua/core/colors.lua
@@ -1,4 +1,6 @@
-vim.cmd.colorscheme("carbonfox")
+require("vague").setup()
+
+vim.cmd.colorscheme("vague")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
