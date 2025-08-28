ff84b0f32da42872edb90451a2a0efc30e93eba3
Dan Hatfield <danhat1020@gmail.com>
2025-08-28 01:05:35 +0100

    Fixed format on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -186,12 +186,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     })
   end,
 })
-vim.api.nvim_create_augroup('AutoFormatting', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.lua',
-  group = 'AutoFormatting',
-  callback = function()
-    vim.lsp.buf.format({ async = true })
-    vim.cmd.write()
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format({ async = false, id = args.data.client_id })
+      end,
+    })
   end,
 })
