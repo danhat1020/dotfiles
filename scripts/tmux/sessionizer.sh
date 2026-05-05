64a89dab0f3a2be9a1355ab44457ebecfefb0e73
Dan Hatfield <danhat1020@gmail.com>
2026-05-05 22:25:35 +0100

    first commit

---
+++ scripts/tmux/sessionizer.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# clears screen after running "tmux-session.sh"
+clear
+
+# sets directories to look in
+DIRS=(
+  "$HOME"
+  "$HOME/.dotfiles"
+  "$HOME/.dotfiles/.config"
+  "$HOME/Documents"
+  "$HOME/Documents"/*/
+  "$HOME/dev"
+  "$HOME/dev"/*/
+)
+
+# looks at all directories specified using the DIRS array, allows user to fuzzy find
+selected=$(fd --type=dir --max-depth=1 --full-path --hidden --exclude ".git" --exclude ".cache" --exclude ".vim" . "${DIRS[@]}" |
+  sed "s|^$HOME/||" |
+  fzf --color=bw --ansi --reverse --prompt="$ ")
+[[ $selected ]] && selected="$HOME/$selected"
+
+# if no directory is selected, exit
+[[ ! $selected ]] && exit 0
+
+# setting name for tmux session
+selected_name=$(basename "$selected" | tr . _)
+
+# if session does not exist
+if ! tmux has-session -t "$selected_name" 2>/dev/null; then
+  # create session
+  tmux new-session -ds "$selected_name" -c "$selected"
+fi
+
+# if tmux is already running, switch to selected session
+if [[ -n $TMUX ]]; then
+  tmux switch-client -t "$selected_name"
+else
+  # if not, attach to selected session
+  tmux attach -t "$selected_name"
+fi
